283bd209fa266363e752dd5fb0b17a163c3b16b0
hanhailong <hanhailong.cool@163.com>
2017-10-22 21:58:35 +0800

    使用conentToString打印集合内容

--- src/com/hhl/algorithm/QuickSort/QuickSort.kt
+++ src/com/hhl/algorithm/QuickSort/QuickSort.kt
@@ -3,13 +3,12 @@ package com.hhl.algorithm.QuickSort
 fun main(args: Array<String>) {
     var source: IntArray = intArrayOf(20, 30, 5, 2, 150, 89, 34, 23, 75, 29, 99)
 
-    for (a in source)
-        print("$a,")
+    //打印原数组
+    println(source.contentToString())
 
     quickSort(source)
     //快速排序之后
-    for (b in source)
-        print("$b,")
+    println(source.contentToString())
 }
 
 /**
@@ -19,5 +18,5 @@ fun main(args: Array<String>) {
  * 3.三数取中，第一个、中间、最后一个取一个中间值
  */
 fun quickSort(array: IntArray) {
-    TODO("快速排序不好理解，明天继续完善")
+//    TODO("快速排序不好理解，明天继续完善")
 }
